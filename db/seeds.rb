82708d9ab52a3c5b3271e3942c571adf0d558b23
Paul Dornfeld <pdornfel@gmail.com>
2013-12-26 10:17:38 -0500

    created categories seed and populated categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require 'csv'
 
-data = Rails.root + 'db/Booksforseeding.csv'
+data1 = Rails.root + 'db/Booksforseeding.csv'
 
-CSV.foreach(data, headers: true) do |row|
+CSV.foreach(data1, headers: true) do |row|
   Book.find_or_create_by(title: row['title'], author: row['author']) do |book|
     book.title = row['title']
     book.author = row['author']
@@ -10,6 +10,16 @@ CSV.foreach(data, headers: true) do |row|
     puts "#{book.title} was inserted into the database"
   end
 end
+
+data2 = Rails.root + 'db/book_categories.csv'
+
+CSV.foreach(data2, headers: true) do |row|
+  Category.find_or_create_by(name: row['name']) do |category|
+    category.name = row['name']
+    puts "#{category.name} was just added to the categories table"
+  end
+end
+
 
 
 
