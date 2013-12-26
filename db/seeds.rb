59e9e1f9fa1ca70e945216236c4abe6e4be23db8
Paul Dornfeld <pdornfel@gmail.com>
2013-12-26 09:57:32 -0500

    added seed for books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+data = Rails.root + 'db/Booksforseeding.csv'
+
+CSV.foreach(data, headers: true) do |row|
+  Book.find_or_create_by(title: row['title'], author: row['author']) do |book|
+    book.title = row['title']
+    book.author = row['author']
+    book.rating = row['rating']
+    puts "#{book.title} was inserted into the database"
+  end
+end
+
+
+
